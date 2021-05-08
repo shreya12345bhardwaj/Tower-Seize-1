dcd6da85aceabd95d1ae81932daa4b65b365dc67
shreya12345bhardwaj <76427422+shreya12345bhardwaj@users.noreply.github.com>
2021-05-08 16:00:56 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,24 @@
+class Block{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.colour = color(random(0,255),random(0,255),random(0,255))
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill(this.colour)
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+}
